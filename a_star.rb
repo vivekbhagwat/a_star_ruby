6e55f310b9ffb4b4bb0523d001f3f15f97adfa70
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-10-17 14:59:36 -0400

    implemented generate_random_square, made n optional argument

--- a_star.rb
+++ a_star.rb
@@ -34,8 +34,60 @@ def h3(square)
   own_dist
 end
 
-def one_dimension_to_two(k)
-  [k / 3, k % 3]
+def generate_random_square(n=3)
+  rand_square = Marshal.load(Marshal.dump(@goal_square))
+  nil_index = [0,0]
+  
+  #Adapted Box-Muller method of generating random numbers.
+  #26 is specific to average for 3x3. Should be changed in future for other n's
+  num_moves = (26 + 10*(Math.sqrt(-2*Math.log(rand)) * Math.cos(2 * Math::PI * rand))).to_i
+  1.upto(num_moves) do
+    possible_directions = [:up,:down,:left,:right]
+    if nil_index[0] == 0
+      possible_directions -= [:up]
+    elsif nil_index[0] == (n-1)
+      possible_directions -= [:down]
+    end
+    
+    if nil_index[1] == 0
+      possible_directions -= [:left]
+    elsif nil_index[1] == (n-1)
+      possible_directions -= [:right]
+    end
+    
+    move = possible_directions[(possible_directions.size * rand).to_i]
+    if move == :up
+      # take val from i-1,j and put in i,j
+      # take nil from i,j and put in i-1,j
+      rand_square[nil_index[0]][nil_index[1]] = rand_square[nil_index[0]-1][nil_index[1]]
+      rand_square[nil_index[0]-1][nil_index[1]] = nil
+      nil_index[0] -= 1
+    elsif move == :down
+      # take val, from i+1, j and put in i,j
+      rand_square[nil_index[0]][nil_index[1]] = rand_square[nil_index[0]+1][nil_index[1]]
+      rand_square[nil_index[0]+1][nil_index[1]] = nil
+      nil_index[0] += 1      
+    elsif move == :left
+      rand_square[nil_index[0]][nil_index[1]] = rand_square[nil_index[0]][nil_index[1]-1]
+      rand_square[nil_index[0]][nil_index[1]-1] = nil
+      nil_index[1] -= 1
+    elsif move == :right
+      rand_square[nil_index[0]][nil_index[1]] = rand_square[nil_index[0]][nil_index[1]+1]
+      rand_square[nil_index[0]][nil_index[1]+1] = nil
+      nil_index[1] += 1  
+    end
+  end
+  
+  p "max moves = #{num_moves}"
+  rand_square
+end
+
+def one_dimension_to_two(k,n=3)
+  [k / n, k % n]
+end
+
+def m(i)
+  Marshal.load(Marshal.dump(i))
 end
 
 square = ([Array.new(3)] << Array.new(3) << Array.new(3))
@@ -46,3 +98,4 @@ p @goal_square.inspect
 
 p h1(test_square)
 p h2(test_square)
+p generate_random_square
