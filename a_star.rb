6328b50710314c8235c33deef8ee69740451f059
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-10-17 03:50:45 -0400

    added h2 function

--- a_star.rb
+++ a_star.rb
@@ -15,7 +15,28 @@ end
 #manhattan distance
 def h2(square)
   man_dist = 0
-  
+  square.each_index do |i|
+    square[i].each_index do |j|
+      if square[i][j]
+        arr = one_dimension_to_two(square[i][j])
+      else
+        arr = [0][0]
+      end
+      
+      man_dist += (arr[0]-i).abs + (arr[1] - j).abs
+    end
+  end
+  man_dist
+end
+
+def two_dimensions_to_one(i,j)
+  3*i + j
+end
+
+def one_dimension_to_two(k)
+  j = k % 3
+  i = k / 3
+  [i,j]
 end
 
 square = ([Array.new(3)] << Array.new(3) << Array.new(3))
@@ -25,3 +46,5 @@ p test_square.inspect
 p @goal_square.inspect
 
 p h1(test_square)
+p h2(test_square)
+p one_dimension_to_two(7)
