a0c6c0fafe6bd30430800d5cef7e20d6dd149ac5
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-10-17 03:20:46 -0400

    added h1 function

---
+++ a_star.rb
@@ -0,0 +1,27 @@
+@goal_square = [[nil,1,2],[3,4,5],[6,7,8]]
+
+def h1(square)
+  count = 0
+  @goal_square.each_with_index do |row,i|
+    row.each_with_index do |cell,j|
+      unless cell == square[i][j]
+        count += 1
+      end
+    end
+  end
+  count
+end
+
+#manhattan distance
+def h2(square)
+  man_dist = 0
+  
+end
+
+square = ([Array.new(3)] << Array.new(3) << Array.new(3))
+test_square = [[nil,1,2],[4,5,3], [6,7,8]]
+p square.inspect
+p test_square.inspect
+p @goal_square.inspect
+
+p h1(test_square)
